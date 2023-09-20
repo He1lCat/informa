6905fbbc319f82e2178cfbad9436909ed570007a
He1lCat <50685828+He1lCat@users.noreply.github.com>
2023-09-20 11:12:48 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,5 +1,31 @@
 import math 
-x = 1
-y = 1
-z = 1
+
+//task1
+x = 3 
+y = 5
+z = 2
 a= (math.log(abs(x-1))-math.sqrt(abs(y)))/((x ** (1. / 3.))+math.sqrt(abs(y)))
+b= (math.log(X))+z ** (x+y)
+print(a,b)
+
+//task2
+a=1
+b=-5
+c=4
+f= ((2*x+b) ** (1. / 3.))+(c*x ** (1. / 3.))/(x+c)
+
+//task3
+x=5
+f=(math.cos(math.sin(1/(x ** (1/3)))) ** 3)
+
+//task5
+v = 1000
+hor = 5
+ras=9*hor*4
+ot= v/ras
+
+//task6
+v=30
+h=5
+r=math.sqrt((v/h)*(4/3))
+l=math.sqrt(r*r+h*h)
