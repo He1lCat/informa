57da7457d93da871a9499d3ce791b333191d4832
He1lCat <50685828+He1lCat@users.noreply.github.com>
2023-09-27 10:08:25 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -13,19 +13,38 @@ a=1
 b=-5
 c=4
 f= ((2*x+b) ** (1. / 3.))+(c*x ** (1. / 3.))/(x+c)
+print(f)
 
 //task3
 x=5
 f=(math.cos(math.sin(1/(x ** (1/3)))) ** 3)
+print(f)
 
 //task5
 v = 1000
 hor = 5
 ras=9*hor*4
 ot= v/ras
+print(ot)
 
 //task6
 v=30
 h=5
 r=math.sqrt((v/h)*(4/3))
 l=math.sqrt(r*r+h*h)
+print(l)
+//task4
+Vch=1000
+Sside=500
+//task9
+cost=2000000
+mon=30000
+while mon not= cost:
+  i=+1
+  if mon>=cost:
+    break
+print(i)
+//
+
+
+
